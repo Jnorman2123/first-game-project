5276afa9b8d33152d80110736d825a184d5a109f
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-13 17:45:26 -0400

    added forward movement for character
    Added ability for the character to move forward

--- First Game Project/Assets/Scripts/PlayerController.cs
+++ First Game Project/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables for character horizontal movement
+    //Variables for character horizontal turning
     public float horizontalInput;
-    public float turnSpeed = 50.0f;
+    public float turnSpeed = 100.0f;
+    //Variables for character forward movement
+    public float forwardInput;
+    public float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Turn character side to side
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+        //Move character forward 
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
     }
 }
