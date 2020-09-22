7d7eac5f4986ea5db4068ddce08ffc5e69c8628b
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-22 15:20:47 -0400

    Removed the coded boundaries
    Removed the coded boundaries from the player controller

--- First Game Project/Assets/Scripts/PlayerController.cs
+++ First Game Project/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,6 @@ public class PlayerController : MonoBehaviour
     public float forwardInput;
     public float horizontalInput;
     public float speed = 10.0f;
-    //Variables for boundaries
-    private float leftBound = 20;
-    private float topBound = 10;
     //Player rigidbody variable 
     private Rigidbody playerRb;
 
@@ -31,19 +28,5 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
 
-        //Keep the player in bounds 
-        if (transform.position.x > leftBound)
-        {
-            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
-        } else if (transform.position.x < -leftBound)
-        {
-            transform.position = new Vector3(-leftBound , transform.position.y, transform.position.z);
-        } else if (transform.position.z > topBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
-        } else if (transform.position.z < -topBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -topBound);
-        }
     }
 }
