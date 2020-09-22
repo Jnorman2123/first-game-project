cbf1040a56edd0694fe69151363d3afb3e7d67df
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-22 15:35:07 -0400

    Adjusted wall length and cleaned up player controller
    Made walls fit together better and cleaned up code in player controller

--- First Game Project/Assets/Scripts/PlayerController.cs
+++ First Game Project/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    //Method to move character based on user input
+    private void MovePlayer()
+    {
         //Turn character side to side
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
@@ -27,6 +33,5 @@ public class PlayerController : MonoBehaviour
         //Move character forward 
         forwardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
-
     }
 }
