1a8c29b458d809e3797ebca0d7aa4046ab365d7d
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-22 14:43:24 -0400

    Added movement to player
    Added player controller script to player and added movement

--- First Game Project/Assets/Scripts/PlayerController.cs
+++ First Game Project/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables for character horizontal turning
-    public float horizontalInput;
-    public float turnSpeed = 100.0f;
-    //Variables for character forward movement
+    //Variables for character movement
     public float forwardInput;
+    public float horizontalInput;
     public float speed = 10.0f;
+    //Variables for boundaries
+    private float leftBound = 20;
+    private float topBound = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +23,25 @@ public class PlayerController : MonoBehaviour
     {
         //Turn character side to side
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       
         //Move character forward 
         forwardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+
+        //Keep the player in bounds 
+        if (transform.position.x > leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        } else if (transform.position.x < -leftBound)
+        {
+            transform.position = new Vector3(-leftBound , transform.position.y, transform.position.z);
+        } else if (transform.position.z > topBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
+        } else if (transform.position.z < -topBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -topBound);
+        }
     }
 }
