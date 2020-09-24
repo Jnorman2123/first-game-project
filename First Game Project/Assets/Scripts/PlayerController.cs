071d5da9270105e73339f77e8764bf03e64b8e27
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-24 10:56:30 -0400

    changed movement of player
    Changed input to get axis raw and created new variable for movement in player controller script

--- First Game Project/Assets/Scripts/PlayerController.cs
+++ First Game Project/Assets/Scripts/PlayerController.cs
@@ -20,18 +20,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Call move player function to move player
         MovePlayer();
     }
 
     //Method to move character based on user input
     private void MovePlayer()
     {
-        //Turn character side to side
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        //Set inputs to vertical and horizontal input
+        forwardInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        //Set movement variable to the player inputs
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, forwardInput); 
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
 
         //Move character forward 
-        forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+        
+        //transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
     }
 }
