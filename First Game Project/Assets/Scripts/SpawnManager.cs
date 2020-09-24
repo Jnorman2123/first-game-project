0c2ffaee12cd7e0438ac78d00f928fd1303eaf89
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-24 18:45:57 -0400

    Added random spawn of regular monster
    One regular monster will spawn in a random location on start

--- First Game Project/Assets/Scripts/SpawnManager.cs
+++ First Game Project/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //Variables for enemy prefabs
+    public GameObject[] enemyPrefabs;
+    //Variable for random spawn location
+    private Vector3 spawnPos;
+    //Variables for spawn ranges
+    private float zRange = 13.5f;
+    private float xRange = 24.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set spawn pos for enemies to random location
+        spawnPos = new Vector3(Random.Range(xRange, -xRange), 0.5f, Random.Range(zRange, -zRange));
+
+        Instantiate(enemyPrefabs[0], spawnPos, enemyPrefabs[0].transform.rotation);
     }
 
     // Update is called once per frame
