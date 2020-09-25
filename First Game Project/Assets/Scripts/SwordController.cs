e0cad9b38400de7e8947d707785d745c17a73fb4
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-25 11:26:05 -0400

    Added ability for sword to knockback enemies
    Added collision function to sword to knockback enemies and adjusted character drags

---
+++ First Game Project/Assets/Scripts/SwordController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    //Variable for sword knock back power 
+    private float knockbackPower = 1000.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 swordKnockback = (collision.gameObject.transform.position - transform.position);
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            enemyRb.AddForce(swordKnockback * knockbackPower, ForceMode.Impulse);
+            Debug.Log(enemyRb);
+        }   
+    }
+}
