000f6834efc44cc182eef2761316af454ca4b845
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-09-29 17:27:53 -0400

    If there are no monsters spawn a wave
    Added ability to spawn monsters when none are left

--- First Game Project/Assets/Scripts/SpawnManager.cs
+++ First Game Project/Assets/Scripts/SpawnManager.cs
@@ -7,30 +7,37 @@ public class SpawnManager : MonoBehaviour
     //Variables for enemy prefabs, boosts, and player
     public GameObject[] enemyPrefabs;
     public GameObject[] boosts;
-    private GameObject player;
     //Variables for random spawn location
     private Vector3 enemySpawnPos;
     private Vector3 boostSpawnPos;
     //Variables for spawn ranges
     private float zRange = 13.5f;
     private float xRange = 24.0f;
+    //Number of monsters variables
+    private GameObject[] regularMonsters;
+    private GameObject[] fastMonsters;
+    private GameObject[] tankMonsters;
+    private int allMonsters;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        WaveOne();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 WeaponPos = player.transform.position + new Vector3(0, 0, 1.5f);
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{ 
-           //Debug.Log("shoot weapon");
-            //Instantiate(weapon, WeaponPos, weapon.transform.rotation);
-        //}
+        regularMonsters = GameObject.FindGameObjectsWithTag("Regular Monster");
+        fastMonsters = GameObject.FindGameObjectsWithTag("Fast Monster");
+        tankMonsters = GameObject.FindGameObjectsWithTag("Tank Monster");
+        allMonsters = regularMonsters.Length + fastMonsters.Length + tankMonsters.Length;
+        if (allMonsters == 0)
+        {
+            int waveNumber = 1;
+            NextWave(waveNumber);
+            waveNumber += 1;
+        }
     }
     //Function to spawn a monster and a random location
     private void spawnMonster(int monsterIndex)
@@ -52,20 +59,41 @@ public class SpawnManager : MonoBehaviour
         //Spawn enemy of given index
         Instantiate(enemyPrefabs[monsterIndex], enemySpawnPos, enemyPrefabs[monsterIndex].transform.rotation);
     }
-
+    //Function to spawn a wave by number and when monsters are dead move to next wave
+    private void NextWave(int wave)
+    {
+       switch (wave)
+        {
+            case 1:
+                WaveOne();
+                break;
+            //case 2:
+                //WaveTwo();
+                //break;
+            //case 3:
+                //WaveThree();
+                //break;
+        }
+    }
+    //Function to spawn the first wave of monsters
     private void WaveOne()
     {
+        //Spawn 5 regular monsters
         for (int i = 0; i < 5; i++) {
             spawnMonster(0);
         }
+        //Spawn a random boost every 10 seconds after 5 second delay
         InvokeRepeating("SpawnBoost", 5.0f, 10.0f);
     }
-
+    //Function to spawn a random boost
     private void SpawnBoost()
     {
+        //Random index to spawn
         int boostIndex = Random.Range(0, boosts.Length);
+        //Variables for spawn position
         float yPos = 0.5f;
         boostSpawnPos = new Vector3(Random.Range(xRange, -xRange), yPos, Random.Range(zRange, -zRange));
+        //Spawn boost
         Instantiate(boosts[boostIndex], boostSpawnPos, boosts[boostIndex].transform.rotation);
     }
 }
