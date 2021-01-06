ceeccd5c6d3f94fb0a17dfdf00053322e8420009
Jonathan Norman <Jnorman2123@gmail.com>
2021-01-05 20:23:43 -0500

    Added code to fireball controller to make the fireball move forward
    Added code to fireball controller to make the fireball move forward

---
+++ First Game Project/Assets/Scripts/FireballController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    // Rigidbody variable for fireball
+    private Rigidbody fireballRigidbody;
+    // Variable for fireball speed
+    public float fireballSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set fireball rigidbody
+        fireballRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveFireball();
+    }
+
+    // Function to move fireball forward
+    private void MoveFireball()
+    {
+        // New vector 3 to set move direction to forward
+        Vector3 fireballMovement = Vector3.forward;
+        // Move fireball according to speed
+        fireballRigidbody.AddForce(fireballMovement * fireballSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
